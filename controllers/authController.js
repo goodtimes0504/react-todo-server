26eb27ed6595ead53c1d8b2e6e5bec647cc0d452
goodtimes0504 <revolution_lc@outlook.com>
2025-01-09 20:31:26 +0800

    feat:implement register function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,19 @@
+const User = require("../models/User")
+
 async function registerUser(req, res) {
-  const data = req.body
-  console.log(data)
-  res.send("success")
+  const { firstName, lastName, username, password } = req.body
+  try {
+    const duplicate = await User.findOne({ username })
+    if (duplicate) {
+      return res.status(400).json({ message: "username already exists" })
+    }
+    const user = new User({ firstName, lastName, username, password })
+    await user.save()
+    res.status(201).json({ message: "user created successfully" })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: "server error" })
+  }
 }
 const AuthController = {
   registerUser,
